9fa3614a5f66f0bd24df151d627eacc90db4da1c
steveologist <steven.camarillo.569@my.csun.edu>
2024-05-02 10:12:22 -0700

    update

--- views/systemview.py
+++
@@ -1,8 +0,0 @@
-"""Display system message."""
-
-
-def display_exit_message():
-    """Display exit message and return a user choice."""
-    print("Type the letter 'q' to confirm stopping the application.: ")
-
-    return input()
